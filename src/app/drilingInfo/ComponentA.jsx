e5eac914c95265f9876fd5b8c3f72d9948705aeb
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-09-19 23:55:08 +0200

    terminado

--- src/app/drilingInfo/ComponentA.jsx
+++ src/app/drilingInfo/ComponentA.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ComponentB from "./ComponentB";
 
 const ComponentA = () => {
-    const numValueX = 42
+  const [numValueX, setNumValueX] = useState(0);
+
+  // const numValueX = 42;
   return (
     <div>
       <div className="flex flex-col gap-2 p-5 items-start w-[700px] h-[400px] bg-slate-200 mb-16 border-[2px] border-black rounded-2xl">
-        <h1 className="text-3xl">ComponentA</h1>
-        <p className="text-xl">x = {numValueX}</p>
+        <div className="flex flex-col gap-2 w-full">
+          <div className="flex gap-5 justify-between">
+            <h1 className="text-3xl">ComponentA</h1>
+            <div className="flex items-center gap-3">
+              <label htmlFor="numValue">Ingrese número</label>
+              <input
+                id="numValue"
+                className="border-[2px] border-black rounded-xl appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none w-14 text-center"
+                type="number"
+                onChange={(e) => setNumValueX(e.target.value)}
+              />
+            </div>
+          </div>
+          <p className="text-xl">x = {numValueX}</p>
+        </div>
 
         <ComponentB numValueX={numValueX} />
       </div>
