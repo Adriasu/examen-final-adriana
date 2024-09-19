75d161b1c16bf676dcb1353d70773baaf8a89225
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-09-19 23:21:46 +0200

    Update ContainCards.jsx

--- src/app/galleryCards/ContainCards.jsx
+++ src/app/galleryCards/ContainCards.jsx
@@ -34,11 +34,7 @@ const ContainCards = () => {
   >
     <h2 className="text-2xl font-medium text-[#2A5B45] leading-7 mb-5">Últimos orígenes</h2>
     <div className={`flex flex-wrap w-[1200px] gap-6`}>
-      {location === "home"
-        ? dataCoffee.slice(0, 4).map((card, i) => {
-            return <CardProduct key={i} data={card} />;
-          })
-        : dataCoffee.map((card, i) => {
+      {dataCoffee.map((card, i) => {
             return <CardProduct key={i} data={card} />;
           })}
     </div>
