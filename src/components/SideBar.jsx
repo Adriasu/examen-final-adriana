41d2bf0623beac5051f217ce24c49ee4c5d9a964
Adriasu <167806166+Adriasu@users.noreply.github.com>
2024-09-19 20:49:05 +0200

    sidebar finalizado

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import {
+  Clock,
+  GalleryHorizontalEnd,
+  HardDriveDownload,
+  IdCard,
+} from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const SideBar = () => {
   return (
-    <div>
-      
+    <div className="border border-black w-[300px] h-screen p-6 ">
+      <div className="flex items-center">
+        <div className="bg-orange-600 text-white py-1 px-1 rounded-full flex flex-col w-[100px]">
+          <p className="font-bold text-center">Examen Final</p>
+        </div>
+      </div>
+
+      <div className="mt-14">
+        <ul className="flex flex-col gap-5">
+          <Link href={"/cardGenerator"}>
+            <div className="flex gap-5">
+              <IdCard />
+              <li>Card Generator</li>
+            </div>
+          </Link>
+
+          <Link href={"/digitalClock"}>
+            <div className="flex gap-5">
+              <Clock />
+              <li>Digital Clock</li>
+            </div>
+          </Link>
+
+          <Link href={"/drilingInfo"}>
+            <div className="flex gap-5">
+              <HardDriveDownload />
+              <li>Driling Info</li>
+            </div>
+          </Link>
+
+          <Link href={"/galleryCards"}>
+            <div className="flex gap-5">
+              <GalleryHorizontalEnd />
+              <li>Galeria de Cartas</li>
+            </div>
+          </Link>
+        </ul>
+      </div>
     </div>
   )
 }
